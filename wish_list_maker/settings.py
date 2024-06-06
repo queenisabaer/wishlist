eb29ac1820f35d4653bd69438934a5a20d2753bf
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-06-06 10:09:12 +0200

    Add static order and connect css file to base.html

--- wish_list_maker/settings.py
+++ wish_list_maker/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
+    '127.0.0.1:8000', 'localhost', '127.0.0.1',
     '.herokuapp.com'
 ]
 
@@ -124,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
