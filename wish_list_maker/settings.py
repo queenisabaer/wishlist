8f5bcd7e4781a718be07f9b568ff21b72d3050e2
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-06-15 14:28:55 +0200

    Style verification pages

--- wish_list_maker/settings.py
+++ wish_list_maker/settings.py
@@ -185,7 +185,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Static files (CSS, JavaScript, Images)
