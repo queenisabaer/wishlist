401738fdb540bcc45b72715f468d346fb609137a
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-06-17 14:46:50 +0200

    Add success message to wish list form

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.contrib import messages
 from .models import WishList
 from .forms import WishListForm
 from django.contrib.auth. mixins import LoginRequiredMixin
@@ -11,7 +12,7 @@ class WishListOverview(generic.ListView):
     context_object_name = "wishlists"
 
 class AddWishList(LoginRequiredMixin, generic.CreateView):
-    """ Add Wishlist view """
+    """ Add a new Wish List view """
     template_name = 'wishlists/add_wishlist.html'
     model = WishList
     form_class = WishListForm
@@ -20,12 +21,14 @@ class AddWishList(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         'Update instance of user'
         form.instance.created_by = self.request.user
+        messages.success(self.request, "Your wish list has been created and can be seen in your profile")
         return super(AddWishList, self).form_valid(form)
     
+
 class WishListDetail(generic.DetailView):
     """ View a single wish list """
     model = WishList
     template_name = "wishlists/wishlist_detail.html"
-    context_object_name = "wishlist"
+    context_object_name = 'wishlist'
     slug_field = 'wish_list_id'  
     slug_url_kwarg = 'wish_list_id' 
