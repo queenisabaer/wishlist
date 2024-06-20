929614439cdc9f2caa6a82f6aa588c91bc3a9a3d
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-06-20 10:22:52 +0200

    Update static files for deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -6,4 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         alert.close();
     }, 3000); // 3000 milliseconds = 3 seconds
 });
-  
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const shareButton = document.getElementById('shareButton');
+    const shareLink = document.getElementById('shareLink');
+    const message = document.getElementById('share-message');
+
+    shareButton.addEventListener('click', async () => {
+        try {
+            await navigator.clipboard.writeText(shareLink.value);
+            message.innerHTML = 'Copied <i class="fa-solid fa-check"></i>';
+            // shows the message only for 2 seconds then switch back to empy p
+            setTimeout(() => {
+                message.innerHTML = '';
+            }, 2000); // 2000 milliseconds = 2 seconds
+        } catch (error) {
+            console.error('Error sharing the link because: ', error);
+            message.innerHTML = 'Failed to copy. Please try again.';
+        }
+    });
+});
